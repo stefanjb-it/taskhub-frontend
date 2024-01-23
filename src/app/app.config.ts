ae964086e613a8ee2effdd565320dcc1bd60b299
Timo Kappel <timo.kappel@gmail.com>
2024-01-23 22:01:03 +0100

    commit for merge

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,7 +5,7 @@ import { routes } from './app.routes';
 import {provideHttpClient, withInterceptorsFromDi} from "@angular/common/http";
 import {JwtModule} from "@auth0/angular-jwt";
 import {ToastrModule, ToastrService} from "ngx-toastr";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {BrowserAnimationsModule, provideAnimations} from "@angular/platform-browser/animations";
 
 export function tokenGetter() {
   return localStorage.getItem('access_token');
@@ -14,23 +14,18 @@ export function tokenGetter() {
 export const appConfig: ApplicationConfig = {
   providers: [
     provideRouter(routes),
-    provideHttpClient(
-      withInterceptorsFromDi()
-    ),
-    importProvidersFrom(
-      JwtModule.forRoot({
+    provideHttpClient(withInterceptorsFromDi()),
+    importProvidersFrom(JwtModule.forRoot({
         config: {
-          tokenGetter: tokenGetter,
-          allowedDomains: ["localhost:8000"],
+            tokenGetter: tokenGetter,
+            allowedDomains: ["localhost:8000"],
         },
-      }),
-      ToastrModule.forRoot({
+    }), ToastrModule.forRoot({
         timeOut: 2000,
         easeTime: 250,
         positionClass: 'toast-top-right',
         preventDuplicates: true,
-      }),
-      BrowserAnimationsModule
-    ),
-  ],
+    }), BrowserAnimationsModule),
+    provideAnimations()
+],
 };
--- src/app/pages/customer-details/customer-details.component.ts
+++ src/app/pages/customer-details/customer-details.component.ts
@@ -10,11 +10,27 @@ import {ActivatedRoute, Router} from "@angular/router";
 import {SimpleInputFieldComponent} from "../../components/simple-input-field/simple-input-field.component";
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {SimpleSelectFieldComponent} from "../../components/simple-select-field/simple-select-field.component";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatIconModule, MatIconRegistry} from "@angular/material/icon";
+import {MatInputModule} from "@angular/material/input";
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import {DomSanitizer} from "@angular/platform-browser";
+
+const THUMBUP_ICON:any =
+  `
+  <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px">
+    <path d="M0 0h24v24H0z" fill="none"/>
+    <path d="M1 21h4V9H1v12zm22-11c0-1.1-.9-2-2-2h-6.31l.95-4.57.03-.32c0-.41-.17-.79-.` +
+  `44-1.06L14.17 1 7.59 7.59C7.22 7.95 7 8.45 7 9v10c0 1.1.9 2 2 2h9c.83 0 1.54-.5` +
+  `1.84-1.22l3.02-7.05c.09-.23.14-.47.14-.73v-1.91l-.01-.01L23 10z"/>
+  </svg>
+`;
 
 @Component({
   selector: 'app-customer-details',
   standalone: true,
-  imports: [CommonModule, ButtonComponent, InputfieldComponent, SelectfieldComponent, SimpleInputFieldComponent, ReactiveFormsModule, SimpleSelectFieldComponent],
+  imports: [CommonModule, ButtonComponent, InputfieldComponent, SelectfieldComponent, SimpleInputFieldComponent, ReactiveFormsModule, SimpleSelectFieldComponent, MatFormFieldModule, MatIconModule, MatInputModule, MatCardModule, MatButtonModule],
   templateUrl: './customer-details.component.html',
   styleUrl: './customer-details.component.scss'
 })
@@ -22,16 +38,18 @@ export class CustomerDetailsComponent  implements OnInit {
   newCustomer : ChangeCustomer = {}
   selection : string | undefined | null;
 
+
   formGroup: FormGroup;
 
   constructor(public userService:UserService, public customerService:CustomerService,
-              private route:ActivatedRoute, private router: Router) {
+              private route:ActivatedRoute, private router: Router, iconRegistry: MatIconRegistry, sanitizer: DomSanitizer) {
     this.formGroup = new FormGroup({
-      name: new FormControl('', [Validators.required]),
+      name: new FormControl('', [Validators.required, Validators.max(50)]),
       address: new FormControl('', [Validators.required]),
       phone: new FormControl('', [Validators.required]),
       is_company: new FormControl(null)
     });
+    iconRegistry.addSvgIconLiteral('thumbs-up', sanitizer.bypassSecurityTrustHtml(THUMBUP_ICON));
   }
 
   ngOnInit(){
