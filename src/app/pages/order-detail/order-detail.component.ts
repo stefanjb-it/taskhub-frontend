50ef5a48d09f09cd87761f3028a3a79c89d5f6d4
Jöbstl Stefan <stefan.joebstl@edu.fh-joanneum.at>
2024-01-23 18:03:32 +0100

    Added reactive forms to order-detail

--- src/app/pages/order-detail/order-detail.component.ts
+++ src/app/pages/order-detail/order-detail.component.ts
@@ -3,17 +3,21 @@ import { CommonModule } from '@angular/common';
 import {ButtonComponent} from "../../components/button/button.component";
 import {InputfieldComponent} from "../../components/inputfield/inputfield.component";
 import {SelectfieldComponent} from "../../components/selectfield/selectfield.component";
-import {ChangeOrder} from "../../models/Order";
+import {ChangeOrder, Order} from "../../models/Order";
 import {Customer} from "../../models/Customer";
 import {UserService} from "../../services/user.service";
 import {OrderService} from "../../services/order.service";
 import {CustomerService} from "../../services/customer.service";
 import {ActivatedRoute, Router} from "@angular/router";
+import {FormControl, FormGroup, ReactiveFormsModule} from "@angular/forms";
+import {combineLatestWith} from "rxjs";
+import {SimpleInputFieldComponent} from "../../components/simple-input-field/simple-input-field.component";
+import {SimpleSelectFieldComponent} from "../../components/simple-select-field/simple-select-field.component";
 
 @Component({
   selector: 'app-order-detail',
   standalone: true,
-    imports: [CommonModule, ButtonComponent, InputfieldComponent, SelectfieldComponent],
+  imports: [CommonModule, ButtonComponent, InputfieldComponent, SelectfieldComponent, ReactiveFormsModule, SimpleInputFieldComponent, SimpleSelectFieldComponent],
   templateUrl: './order-detail.component.html',
   styleUrl: './order-detail.component.scss'
 })
@@ -21,63 +25,76 @@ export class OrderDetailComponent implements OnInit {
   newOrder : ChangeOrder = {}
   customers: Customer[] = [];
   selection : string | undefined | null;
+  formGroup: FormGroup;
+  order: Order | undefined;
+  states: any[] = [
+    {
+      id: "false",
+      name: "No"
+    },
+    {
+      id: "true",
+      name: "Yes"
+    }
+  ];
+
   constructor(public userService:UserService, public customerService:CustomerService,
               public orderService:OrderService, private route:ActivatedRoute, private router: Router) {
-
+    this.formGroup = new FormGroup( {
+      order_nr: new FormControl(''),
+      title: new FormControl(''),
+      order_date: new FormControl(''),
+      customer: new FormControl(null),
+      is_completed: new FormControl(null)
+    })
+    this.formGroup.valueChanges.subscribe((value) => {
+      //console.log(value)
+    });
   }
 
   ngOnInit() {
-    this.customerService.getCustomers().subscribe((customers: Customer[]) => {
-      this.customers = customers;
-    });
     this.selection = this.route.snapshot.paramMap.get('id');
-    if (this.selection) {
-      this.orderService.getOrder(parseInt(this.selection)).subscribe(order => {
-        this.newOrder.title = order.title
-        this.newOrder.order_nr = order.order_nr
-        this.newOrder.order_date = order.order_date.length > 10 ? order.order_date.substring(0, 10) : order.order_date
-        this.newOrder.customer = order.customer.id
-        this.newOrder.is_completed = order.is_completed
-      })
+    if (!this.selection) {
+      this.customerService.getCustomers().subscribe((customers: Customer[]) => {
+        this.customers = customers;
+        this.states = [
+          {
+            id: "false",
+            name: "No"
+          },
+          {
+            id: "true",
+            name: "Yes"
+          }
+        ]
+      });
+      this.states = [false, true]
+      return;
     }
+    this.orderService.getOrder(parseInt(this.selection)).pipe(
+      combineLatestWith(
+        this.customerService.getCustomers()
+      )
+    ).subscribe(([order, customers]) => {
+      //console.log(order, customers)
+      this.order = order;
+      this.customers = customers
+
+      this.formGroup.patchValue(order)
+      this.formGroup.controls['customer'].setValue(order.customer?.id)
+      this.formGroup.controls['is_completed'].setValue(order.is_completed)
+    })
   }
 
-  getOrderNumber($event: string) {
-    this.newOrder.order_nr = parseInt($event);
-  }
-
-  getTitle($event: string) {
-    this.newOrder.title = $event;
-  }
-
-  getDate($event: string) {
-    this.newOrder.order_date = $event;
-  }
-
-  getCustomer($event: string) {
-    if ($event != '-1') {
-      this.newOrder.customer = parseInt($event);
-      console.log(this.newOrder.customer);
+  toName(input: Customer):string | null {
+    if (input == undefined) {
+      return null;
     }
+    return input.name;
   }
 
-  getStatusId(isCompleted:boolean) : number {
-    if (isCompleted) {
-      return 1
-    } else {
-      return 0
-    }
-  }
-
-  getStatus($event: string) {
-    if ($event == 'Yes') {
-      this.newOrder.is_completed = true;
-    } else {
-      this.newOrder.is_completed = false;
-    }
-  }
-
-  createOrEditOrder() {
+  handleSubmit() {
+    this.newOrder = this.formGroup.value;
     if (this.selection) {
       this.orderService.changeOrder(parseInt(this.selection), this.newOrder).subscribe(
         res => {
@@ -99,5 +116,4 @@ export class OrderDetailComponent implements OnInit {
         });
     }
   }
-
 }
